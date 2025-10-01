cbf37c7629eb2e94b8ce5ba354bceda3215e09de
Adiyakhuu Uuriintsolmon <uadiya1012@gmail.com>
2025-10-01 13:46:11 +0800

    session_12

---
+++ session_12/day_2_demo.py
@@ -0,0 +1,8 @@
+#age = input("Та хэдэн настай вэ?")
+#age10 = int(age) + 10
+#print(f"{age10}")
+
+length = input("тэгш өнцөгтийн урт ")
+width = input("тэгш өнцөгтийн өргөн ")
+s = float(length) * float(width)
+print(s)
