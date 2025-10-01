24b4640b4ab1ba852615c8ebc643543f1fe12112
Adiyakhuu Uuriintsolmon <adiyakhuuuuriintsolmon@Adiyakhuus-MacBook-Pro.local>
2025-10-01 18:21:23 +0800

    session_12

---
+++ session_12/practice.py
@@ -0,0 +1,11 @@
+bill = float(input("Нийт төлбөрийн дүнгээ оруулна уу: "))
+tip_percentage = float(input("Хэдэн хувийн гарын мөнгө өгөх вэ? (жишээ нь: 15): "))
+tip_amount = tip_percentage * bill / 100
+total_bill = tip_amount + bill
+print(f"""
+      --- Төлбөрийн тайлан ---
+      Үндсэн төлбөр: {bill}₮
+      Гарын мөнгө (10.0%): {tip_amount}₮
+      ----------------------------------
+      Нийт төлөх дүн: {total_bill}₮
+      """)
