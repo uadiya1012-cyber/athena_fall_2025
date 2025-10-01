35bec9efb5f389466e5ad4e3a9c96807f914e0cc
Adiyakhuu Uuriintsolmon <adiyakhuuuuriintsolmon@Adiyakhuus-MacBook-Pro.local>
2025-10-01 18:00:40 +0800

    session_12

---
+++ session_12/python_exercises.py
@@ -0,0 +1,115 @@
+#1
+# age_str = input("How old are you? ")
+# age_int = int(age_str)
+# new_age = age_int + 5
+# print(f"5 жилийн дараах нас: {new_age} ")
+
+#2
+# height = float(input("Та өндөр өө оруулна уу: (метрээр) "))
+# new_height = height + 0.1
+# print(f"Таны шинэ өндөр {new_height}метр ") 
+
+#3
+# fav_number = int(input("Дуртай тоогоо оруулна уу: "))
+# print(f"Таны дуртай тоо бол {fav_number}")
+
+#4
+# weight = float(input("Жингээ оруулна уу: "))
+# integer = int(weight)
+# print(f"Таны бүхэл жин: {integer} ")
+
+#5
+# num1 = int(input("Эхний тоо: "))
+# num2 = int(input("Хоёр дахь тоо: "))
+# sum = num1 + num2
+# print(f"Нийлбэр: {sum} ")
+
+#6
+# num1 = float(input("Эхний тоо оруулна уу: "))
+# num2 = float(input("Хоёр дахь тоо оруулна уу: "))
+# multiplication = num1 * num2
+# print(f"Хоёр тооны үржвэр: {multiplication} ")
+
+#7
+# num = int(input("Тоо оруулна уу: "))
+# square = num * num
+# print(f"Квадрат: {square}")
+
+#8
+# num = int(input("Тоо оруулна уу: "))
+# cube = num * num * num
+# print(f"Куб: {cube} ")
+
+#9
+# Celsium = float(input("Цельсийн хэм оруулна уу: "))
+# Fahrenheit = (Celsium * 9 / 5) + 32
+# print(f"Фаренгейт: {Fahrenheit} ")
+
+#10
+# fahrenheit = float(input("Фаренгейтийн хэм оруулна уу: "))
+# Celsium = (fahrenheit - 32) * 5 / 9
+# print(f"Цельс: {Celsium} ")
+
+#11
+# base = float(input("Суурь уртыг оруулна уу: "))
+# height = float(input("Өндөр оруулна уу: "))
+# area = (base * height) / 2
+# print(f"Гурвалжны талбай: {area} ")
+
+#12
+# num1 = int(input("1-р тоо оруулна уу: "))
+# num2 = int(input("2-р тоо оруулна уу: "))
+# num3 = int(input("3-р тоо оруулна уу: "))
+# avg = float(num1 + num2 + num3) / 3
+# print(f"3-н тооны дундаж: {avg} ")
+
+#13
+# C = float(input("Цельсийн хэм оруулна уу: "))
+# F = (C * 9 / 5) + 32
+# print(f"Фаренгейт: {F} ")
+
+#14
+# num = int(input("Тоо оруулна уу: "))
+# result = num * 2
+# print(f"Таны тоо 2-оор үржигдсэн: {result} ")
+
+#15
+# num = int(input("Эхний тоог оруулна уу: "))
+# sum = num + (num + 1) + (num +2)
+# print(f"Дараалсан 3 тооны нийлбэр: {sum} ") 
+
+#16
+# l = float(input("Баганын уртыг оруулна уу: "))
+# w = float(input("Баганын өргөнийг оруулна уу: "))
+# h = float(input("Баганын өндөр оруулна уу: "))
+# vol = l * w * h 
+# print(f"Баганын эзэлхүүн: {vol} ")
+
+#17
+# min = int(input("Минут оруулна уу: "))
+# seconds = min * 60
+# print(f"Секунд: {seconds} ")
+
+#18
+# p1 = float(input("Бараа 1-ийн үнийг оруулна уу: "))
+# p2 = float(input("Бараа 2-ийн үнийг оруулна уу: "))
+# p3 = float(input("Бараа 3-ийн үнийг оруулна уу: "))
+# total = p1 + p2 + p3
+# avg = total / 3
+# print(f"""
+#       Нийт үнэ: {total}
+#       Дундаж үнэ: {avg}
+#       """)
+
+#19
+# a = float(input("Эхний тоо оруулна уу: "))
+# b = float(input("Хоёр дахь тоо оруулна уу: "))
+# q = a / b
+# print(f"Хуваалт: {q} ")
+
+#20
+# a = int(input("Эхний тоо оруулна уу: "))
+# b = int(input("Хоёр дахь тоо оруулна уу: "))
+# r = a % b
+# print(f"Хоёр тооны үлдэгдэл: {r} ")
+
