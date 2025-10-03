b5b55b08558c16b4243f3d4cc4c6bc245968691d
Adiyakhuu Uuriintsolmon <adiyakhuuuuriintsolmon@Adiyakhuus-MacBook-Pro.local>
2025-10-03 10:43:50 +0800

    session_14

---
+++ week03/session_14/loop_intro.py
@@ -0,0 +1,11 @@
+# for i in range(1, 6):
+#     print(i)
+
+# sum = 0
+# for i in range(1, 101):
+#     sum = sum + i
+# print(sum)
+
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         print(i)
---
+++ week03/session_14/recapture.py
@@ -0,0 +1,29 @@
+# Yesterday Recap
+
+# Дасгал
+# хэрвээ би 14 настай бол teenager үгүй бол насанд хүрсэн гэдэг код бичнэ үү
+
+# Оюутанууд оноо авчээ. Оюутануудын оноог хэд авснаас шалтгаалан оноог нь Failed эсвэл Success
+# гэж боддог болгоно уу. Тэнцэх оноо 60
+
+# Хэрвээ би Монгол улсын иргэн бас Монгол пасспорттой бол Монгол хүн мөн гэж хэвэлдэг болгох
+# бусад үед хэвлэхгүй
+
+
+# age = int(input("How old are you? "))
+# if age > 14
+#     print("Adult")
+# else:
+#     print("Teenager")
+
+# Score = int(input("Your score "))
+# if Score == 60:
+#     print("Success")
+# else:
+#     print("Failed")
+
+# passport = input("Have you got a Mongolian passport? ")
+# if = Mongolian or Have passport:
+#     print("Mongol hun")
+# else:
+#     print("")
