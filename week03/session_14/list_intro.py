5e08368cd30baf108544fe241dcdfc713c4f8f25
Adiyakhuu Uuriintsolmon <uadiya1012@gmail.com>
2025-10-03 10:43:50 +0800

    session_14

---
+++ week03/session_14/list_intro.py
@@ -0,0 +1,60 @@
+# Problem
+# 100 turliin huwisagch uusgeed hadgalna uu
+# ymr ymr asuudaluud uuseh we
+# 1. mash ih huwisagch zarlana
+# 2. hugatsaa -200sec => 3 minute
+
+# Solution
+# Python list
+
+# fruits = ["Apple", "Pear", "Strawberry"]
+# print(fruits)
+
+# # index - 0, 1, 2
+# print("Adiya fav fruit")
+# Adiya_fav_fruit = fruits
+# print(Adiya_fav_fruit)
+
+# #change items
+# fruits[1] = "Orange"
+# print(fruits)
+
+# #add items
+# fruits.append("Blueberry")
+# print(fruits)
+# fruits.insert(2, "Banana")
+# print(fruits)
+
+# #remove item
+# fruits.remove("Apple")
+# print(fruits)
+
+# fruits.pop(2)
+# print(fruits)
+
+# # loop through list
+# for fruit in fruits:
+#     print(fruit)
+
+# #check if item exists
+# if "Apple" in fruit:
+#     print("Yes apple is in the list")
+
+# # list kenght
+# print(len(fruits))
+
+# # sort list
+# numbers = [1, -5, 0, 15, 100, 7]
+# print(f"Not sorted list {numbers}")
+# numbers.sort()
+# print(f"Sorted list {numbers} ")
+
+# # x | 0 | x
+# # 0 | x | 0
+# # 0 | x | 0 
+
+# tic_tac_toe_game_board = [["x", "0", "x"], ["0", "x", "0"], ["0", "x", "0"]]
+# #first line
+# print(tic_tac_toe_game_board[0])
+# # first line first x
+# print(tic_tac_toe_game_board[0] [0])
