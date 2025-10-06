671d86f762a3e02c65614e8179e96416a022dda7
Adiyakhuu Uuriintsolmon <uadiya1012@gmail.com>
2025-10-06 19:22:48 +0800

    session_15

---
+++ week03/session_15/project.py
@@ -0,0 +1,12 @@
+# import random
+# secret_number = random.randint(1, 100)
+# print("Би 1-100-ийн хооронд тоо нуусан. Та таана уу! ")
+# while True:
+#     guess = int(input("Таамаглалаа оруулна уу: "))
+#     if guess == secret_number:
+#         print("Баяр хүргье! та зөв таалаа.")
+#         break
+#     elif guess < secret_number:
+#         print("Арай бага байна")
+#     else:
+#         print("Арай их байна")    
--- week03/session_15/while_loop_intro.py
+++ week03/session_15/while_loop_intro.py
@@ -1,18 +1,18 @@
 # WHILE loop intro
 
-user_name = input("Give me your name: ")
+# user_name = input("Give me your name: ")
 
-while user_name == "Adiya":
-    print(f"Sainuu {user_name}")
-    user_name = "The end"
+# while user_name == "Adiya":
+#     print(f"Sainuu {user_name}")
+#     user_name = "The end"
 
-print("out of while loop")
+# print("out of while loop")
 
-counter = 0
-while True:
-    print(f"Counter is : {counter}")
-    if counter > 10:
-        break
-    else:
-        print(f"Counter is : {counter}.")
-        counter = counter + 1
+# counter = 0
+# while True:
+#     print(f"Counter is : {counter}")
+#     if counter > 10:
+#         break
+#     else:
+#         print(f"Counter is : {counter}.")
+#         counter = counter + 1
