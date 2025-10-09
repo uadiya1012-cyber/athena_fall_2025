b9772f4f4ec75911b3db507a1236af411af9d2f9
Adiyakhuu Uuriintsolmon <uadiya1012@gmail.com>
2025-10-09 10:21:47 +0800

    week04/session18

--- session_11/flowchart4.py
+++
@@ -1,6 +0,0 @@
-length = int(input("тэгш өнцгийн урт хэд вэ?: "))
-print (length)
-width = int(input("тэгш өнцгийн өргөн хэд вэ?: "))
-print (width)
-square = length*width
-print (square)
--- week03/session_11/session_13/many_conditions_03.py
+++
@@ -1,6 +0,0 @@
-day = "Sunday"
-is_sunny = False
-if day == "Sunday" or day == "Saturday" or is_sunny == True:
-    print("go to park ")
-else:
-    print("go to home ")    
