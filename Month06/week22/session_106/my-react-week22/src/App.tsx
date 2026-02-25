17ef76a703fdbb5d64c10d380e7bdfafa5be22b9
Adiyakhuu Uuriintsolmon <uadiya1012@gmail.com>
2026-02-25 12:20:34 +0800

    add lesson for pagination

--- Month06/week22/session_106/my-react-week22/src/App.tsx
+++ Month06/week22/session_106/my-react-week22/src/App.tsx
@@ -2,32 +2,32 @@ import { useState } from 'react'
 import './App.css'
 import { LoginPage } from './LoginPage';
 import { TasksPage } from './TasksPage';
-import CounterExercise from './components/exercises/CounterExerise';
-import FormExercise from './components/exercises/FormExercise';
-import FilterListExercise from './components/exercises/FilterListExercise';
-import AccordionExercise from './components/exercises/AccordionExercise';
-import TasksLabExercise from './components/exercises/TasksLabExercise';
-import LoginExercise from './components/exercises/LoginExercise';
+// import CounterExercise from './components/exercises/CounterExerise';
+// import FormExercise from './components/exercises/FormExercise';
+// import FilterListExercise from './components/exercises/FilterListExercise';
+// import AccordionExercise from './components/exercises/AccordionExercise';
+// import TasksLabExercise from './components/exercises/TasksLabExercise';
+// import LoginExercise from './components/exercises/LoginExercise';
 
 function App() {
   const [isAuth, setIsAuth] = useState(
     !!localStorage.getItem('token')
   )
-  // return isAuth ? (
-  //   <TasksPage onLogout={() => { localStorage.removeItem('token'); setIsAuth(false) }} />
-  // ) : (
-  //   <LoginPage onLogin={() => setIsAuth(true)} />
-  // )
-  return (
-    < main >
-      <CounterExercise />
-      <FormExercise />
-      <FilterListExercise />
-      <AccordionExercise />
-      <TasksLabExercise />
-      <LoginExercise />
-    </main >
+  return isAuth ? (
+    <TasksPage onLogout={() => { localStorage.removeItem('token'); setIsAuth(false) }} />
+  ) : (
+    <LoginPage onLogin={() => setIsAuth(true)} />
   )
+  // return (
+  //   < main >
+  //     <CounterExercise />
+  //     <FormExercise />
+  //     <FilterListExercise />
+  //     <AccordionExercise />
+  //     <TasksLabExercise />
+  //     <LoginExercise />
+  //   </main >
+  // )
 }
 
 export default App
--- Month06/week22/session_106/week22-backend/tasks/views.py
+++ Month06/week22/session_106/week22-backend/tasks/views.py
@@ -4,7 +4,7 @@ from .models import Task
 from .serializers import TaskSerializer
 
 class TaskViewSet(ModelViewSet):
-    queryset = Task.objects.all()
+    queryset = Task.objects.all().order_by('-created_at')
     serializer_class = TaskSerializer
     filterset_fields = ['status']
     search_fields = ['title', 'description']
